7371b9a304ccb63b7ee109c3fbbe988de8104b33
mahmoud-abu-sultan <mahmoudabosultan.t@gmail.com>
2023-03-25 18:13:18 +0300

    init up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+// ------------------------
+// import "bootstrap/dist/css/bootstrap.css";
+// -----------------------
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
