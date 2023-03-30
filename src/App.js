9f24e1f421859450a60465707c499b3986243974
MAHMOUD ABU SULTAN - 1M.T⚛️ <95148656+mahmoud-abu-sultan@users.noreply.github.com>
2023-03-30 17:07:48 +0300

    fint up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-// ------------------------
-// import "bootstrap/dist/css/bootstrap.css";
 // -----------------------
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Card(props) {
-  const handelCategori = () => {
-    // {props.articleTitle};
-  };
   return (
     <article className="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">
       <figure>
@@ -13,12 +10,9 @@ function Card(props) {
         </Link>
       </figure>
       <span className="fh5co-meta">
-        <Link to="/CategorieNews" onClick={handelCategori}>
-          {props.categoris}
-        </Link>
+        <Link to="/CategorieNews">{props.categoris}</Link>
       </span>
       <h2 className="fh5co-article-title" style={props.style}>
-        {/* <Link to="/DiscrNews">{props.articleTitle}</Link> */}
         {props.articleTitle}
       </h2>
       <span className="fh5co-meta fh5co-date">March 6th, 2016</span>
